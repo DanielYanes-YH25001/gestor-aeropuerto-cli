770e6a6fafaf500a26769ea0b53d21d74c2a53d6
DanielYanes-YH25001 <yh25001@ues.edu.sv>
2026-05-27 15:02:53 -0600

    style: Corrección de estilo en archivos
    Corrección en el estilo de algunos archivos para utilizar la convención
    de estilo de Python, agregando dos líneas por cada bloque principal

--- classes/NodoAsiento.py
+++ classes/NodoAsiento.py
@@ -1,5 +1,6 @@
 from classes.Pasajero import Pasajero
 
+
 class NodoAsiento:
   """Nodo para una lista enlazada de asientos y pasajeros."""
   def __init__(self, n_asiento: str = None, pasajero: Pasajero = None):
