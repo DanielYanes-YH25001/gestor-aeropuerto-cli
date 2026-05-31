4f6dd801cfa5f9a0566b957e40ed10313e57868b
Carlos Ayala <am25008@ues.edu.sv>
2026-05-30 22:29:25 -0600

    feat: se agrega test con mock a obtener_fecha_hora_actual

--- tests/test_historial_vuelos.py
+++ tests/test_historial_vuelos.py
@@ -1,5 +1,7 @@
 from modules.historial_vuelos import obtener_fecha_hora_actual
+from unittest.mock import patch, Mock
 import unittest
+
 
 class TestObtenerFechaHoraActual(unittest.TestCase):
     
@@ -15,6 +17,27 @@ class TestObtenerFechaHoraActual(unittest.TestCase):
         
         self.assertTrue(resultado["fecha"])
         self.assertTrue(resultado["hora"])
+        
+    # Verifica que la fecha y hora se obtienen correctamente usando mock
+    @patch("modules.historial_vuelos.datetime")
+    def test_obtener_fecha_hora_actual(self, mock_datetime):
+        mock_fecha_hora = Mock()
+        mock_fecha_hora.day = 30
+        mock_fecha_hora.month = 5
+        mock_fecha_hora.year = 2026
+        mock_fecha_hora.hour = 15
+        mock_fecha_hora.minute = 30
+        mock_fecha_hora.second = 25
+
+        mock_datetime.now.return_value = mock_fecha_hora
+
+        resultado = obtener_fecha_hora_actual()
+
+        self.assertEqual(resultado, {
+            "fecha": "30/5/2026",
+            "hora": "15:30:25"
+        })
+        
         
 if __name__ == '__main__':
     unittest.main()
